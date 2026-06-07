0941e0afe267dc957becbbc65d98460fa8bee830
Creative3Bx <Creative3Bx@gmail.com>
2026-06-07 17:27:21 +1000

    fix pipline issue

--- pages/_app.js
+++ pages/_app.js
@@ -54,24 +54,24 @@ const App = ({ Component, pageProps }) => {
   });
   return (
     <>
+      {/* Google Tag Manager - Global site tag */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}');
+          `,
+        }}
+      />
       <Head>
-        {/* Google Tag Manager - Global site tag */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}`}
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TAG_ID}');
-            `,
-          }}
-        />
         {/* Dynamically build JSON-LD from config */}
         <script
           type="application/ld+json"
